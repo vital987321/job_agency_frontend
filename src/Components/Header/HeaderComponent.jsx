21678632cf1b5825beb9ffcd8924b7ab72faa6d6
vital987321 <vitaliyvelichko@gmail.com>
2024-03-28 23:22:51 +0100

    header layout

--- src/Components/Header/HeaderComponent.jsx
+++ src/Components/Header/HeaderComponent.jsx
@@ -1,27 +1,29 @@
+import agency_logo from "../../img/CONTROL_Agency_logo.png"
+
 export const HeaderComponent = () => {
-    return <header>
+    return (
+      <header>
         <div className="header">
-            <div className="logo">
-                
-            </div>
-            <div className="header-Menu">
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-            <div className="headerControls" >
-                <ul>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
+          <div className="header-logo">
+            <img src={agency_logo} height={80} alt="logo" />
+          </div>
+
+          <ul className="header-menu">
+            <li>Vacancies</li>
+            <li>About us</li>
+            <li>Rewiews</li>
+            <li>Certificates</li>
+            <li>Contacts</li>
+          </ul>
+
+          <div className="header-controls">
+            <button className="header-button">LogIn</button>
+            <button className="header-button">User</button>
+          </div>
         </div>
         <h2> Routes</h2>
         <h3 className="textCenter">Header</h3>
         <hr />
-    </header>
+      </header>
+    );
 };
