e1ff869cb8aefe83461abe51299a16f574ab481a
vital987321 <vitaliyvelichko@gmail.com>
2024-03-29 23:38:43 +0100

    Rourers added

--- src/Components/Main/VacanciesComponent.jsx
+++ src/Components/Main/VacanciesComponent.jsx
@@ -1,3 +1,19 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
 export const VacanciesComponent = () => {
-  return <div> List of Vacancies</div>;
+  const [data, setData]=useState([])
+  useEffect(() => {
+    axios
+      // .get("https://jsonplaceholder.typicode.com/users")
+      .get("http://127.0.0.1:8000/vacancy")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    },[]
+  )
+  return (
+    <div className="vacancies-container">
+
+    </div>
+  );
 };
