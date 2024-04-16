3e0b2e016501538764b934474810ddcb5cc675c5
vital987321 <vitaliyvelichko@gmail.com>
2024-04-16 17:13:40 +0200

    working on logout

--- src/Components/Main/UserProfileComponent.jsx
+++ src/Components/Main/UserProfileComponent.jsx
@@ -1,24 +1,95 @@
-import { jwtDecode } from "jwt-decode";
-// import "core-js/stable/atob";
-// import { decode } from "base-64";
-// global.atob = decode;
+import { useEffect, useState } from "react";
+import api from "../api";
+
+export const UserProfileComponent = () => {
+  const [userData, setUserData] = useState(null);
+  const [userCurrentData, setUserCurrentData] = useState({});
+  const [isUserDataChanged, setIsUserDataChanged] = useState(false);
+
+  const user_id = JSON.parse(localStorage.getItem("user_id"));
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get("/user/" + user_id);
+        setUserData(response.data);
+        setUserCurrentData(response.data);
+        // console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchProfile();
+  }, []);
 
 
-export const UserProfileComponent=()=>{
+
+  const inputChangeHandler = (e) => {
+    let updatedItem = {};
+    const itemKey = e.target.dataset.key;
+    const intemValue = e.target.value;
+    updatedItem[itemKey] = intemValue;
+    setUserCurrentData({
+      ...userCurrentData,
+      ...updatedItem,
+    });
     
-const t=localStorage.getItem('token')
-console.log(t)
-// const d=jwtDecode(t)
-// console.log(d)
+    setIsUserDataChanged(true);
+  };
 
-    return <>
-        <table>
-            <tbody>
-                <tr>
-                    <td>email:</td>
-                    <td>gg</td>
-                </tr>
-            </tbody>
-        </table>
-    </>
-}
+  const SubmitUserProfileChangesComponent=()=>{
+    if (isUserDataChanged){
+        return <>
+            <input type="submit" className="button-common" value="Save changes"/>
+        </>
+    }
+  }
+
+
+  if (userData) {
+    return (
+      <>
+        <form>
+          <input
+            id="user-profile-email-input"
+            data-key="email"
+            type="text"
+            placeholder="email"
+            onChange={inputChangeHandler}
+            value={userCurrentData.email}
+          />
+          <input
+            id="user-profile-first-name-input"
+            data-key="first_name"
+            type="text"
+            placeholder="First name"
+            onChange={inputChangeHandler}
+            value={userCurrentData.first_name ? userCurrentData.first_name : ""}
+          />
+          <input
+            id="user-profile-last-name-input"
+            data-key="last_name"
+            type="text"
+            placeholder="Last name"
+            onChange={inputChangeHandler}
+            value={userCurrentData.last_name ? userCurrentData.last_name : ""}
+          />
+          <input
+            id="user-profile-phone-input"
+            data-key="phone"
+            type="text"
+            placeholder="Phone number"
+            onChange={inputChangeHandler}
+            value={userCurrentData.phone ? userCurrentData.phone : ""}
+          />
+        <SubmitUserProfileChangesComponent/>
+
+        </form>
+      </>
+    );
+
+  }
+  else{
+    return <div>User not found</div>
+  }
+};
