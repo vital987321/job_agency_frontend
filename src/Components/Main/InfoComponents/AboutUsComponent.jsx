1dd1bbf37b4ad46366dc9e61b7a467d37629ae19
vital987321 <vitaliyvelichko@gmail.com>
2024-04-23 20:40:47 +0200

    Add about Component

--- src/Components/Main/InfoComponents/AboutUsComponent.jsx
+++ src/Components/Main/InfoComponents/AboutUsComponent.jsx
@@ -5,15 +5,18 @@ import icon_safe_52 from '../../../svg/icon-safe 52.svg'
 import icon_support_52 from '../../../svg/icon-support 52.svg'
 import smile_icon from '../../../svg/Smile_icon.svg'
 import man1 from '../../../img/man1.png'
+import man2 from '../../../img/man2.png'
+import man3 from '../../../img/man3.png'
+import man4 from '../../../img/man4.png'
 
 import "../../../css/infoComponents/aboutUs.css"
 export const AboutUsComponent=()=>{
     return<>
     <div className='about-us-component-main-container'>
         <section className="about-us-section">
-            <h2 className='about-us-header'>
+            <h1 className='about-us-header'>
                 About us
-            </h2>
+            </h1>
             <div className="about-us-text-container">
                 <div>
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis iste cupiditate, incidunt obcaecati eos praesentium mollitia veritatis amet dolore voluptate. Non exercitationem accusantium commodi animi nulla dolor dolore inventore harum? Ipsum error asperiores sequi eos, id voluptates ea distinctio, exercitationem perferendis dolores, ducimus laudantium deleniti eveniet laboriosam provident! Eaque totam, ut commodi corrupti sit quisquam repellat unde, velit molestiae eligendi, quis repudiandae sequi! Commodi iste, consequatur provident esse magni tempore voluptas distinctio quae deleniti voluptatibus corrupti natus alias eius laboriosam dolor rem inventore! Iure autem debitis nemo aut esse quaerat quae, blanditiis ad sed earum vitae assumenda quo sunt at.
@@ -25,9 +28,9 @@ export const AboutUsComponent=()=>{
         </section>
         
         <section className="why-us-section">
-            <h3 className='why-us-header'>
+            <h2 className='why-us-header'>
                 Why to choose us?
-            </h3>
+            </h2>
             <p className='why-us-subheader'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vel, unde tenetur hic quaerat quidem veritatis repellat ad consequatur libero voluptates deleniti nisi exercitationem?
             </p>
@@ -108,20 +111,33 @@ export const AboutUsComponent=()=>{
         </section>
 
         <section className='our-team-section'>
-            <h3 className='our-team-section-header'>
+            <h2 className='our-team-section-header'>
                 Our team
-            </h3>
+            </h2>
 
             <ul className='our-team-members-container'>
-                <li className='out-team-member-card'>
-                    <img src={man1} alt="pfoto" />
-                    <p>Martin Ludek</p>
-                    <p>Financial director</p>
+                <li className='our-team-member-card'>
+                    <img className='our-team-member-card-photo' src={man1} alt="pfoto" />
+                    <p className='our-team-member-card-name'>Martin Ludek</p>
+                    <p className='our-yeam-member-card-position'>CEO</p>
+                </li>
+                <li className='our-team-member-card'>
+                    <img className='our-team-member-card-photo' src={man2} alt="pfoto" />
+                    <p className='our-team-member-card-name'>Franra Omacka</p>
+                    <p className='our-yeam-member-card-position'>Financial director</p>
+                </li>
+                <li className='our-team-member-card'>
+                    <img className='our-team-member-card-photo' src={man3} alt="pfoto" />
+                    <p className='our-team-member-card-name'>Olga May</p>
+                    <p className='our-yeam-member-card-position'>HR specialist</p>
+                </li>
+                <li className='our-team-member-card'>
+                    <img className='our-team-member-card-photo' src={man4} alt="pfoto" />
+                    <p className='our-team-member-card-name'>Vanesa Victoria</p>
+                    <p className='our-yeam-member-card-position'>Manager</p>
                 </li>
             </ul>
         </section>
     </div>
-        
-
     </>
 }
