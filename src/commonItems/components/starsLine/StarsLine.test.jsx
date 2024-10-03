7e10cb41fc560a500efd320ecaad6d032adcb101
vital987321 <vitaliyvelichko@gmail.com>
2024-10-03 22:41:07 +0200

    Added StarsLine.test.jsx

--- src/commonItems/components/starsLine/StarsLine.test.jsx
+++ src/commonItems/components/starsLine/StarsLine.test.jsx
@@ -4,13 +4,36 @@ import { StarsLine } from "./StarsLine";
 
 describe("StarsLine", () => {
     it("returns 5 empty stars on rating=0", () => {
-        render(<StarsLine rating={1} />)
+        const rating=0
+        render(<StarsLine rating={rating} />)
         const result = screen.getAllByRole("img")
-        screen.debug()
         expect(result).toHaveLength(5)
-        const result2 = screen.get("img", {
-          source: "/src/assets/svg/rating_star_icon_yellow.svg",
-        });
-        expect(result2).toHaveLength(1);
-    })
+        result.forEach((value)=>{
+          expect(value).toHaveAttribute('src', expect.stringContaining('white'))
+        })
+    });
+    it('returns 3 full stars and 2 empty stars on rating=3.6', ()=>{
+      const rating=3.6
+      render(<StarsLine rating={rating} />)
+      const result = screen.getAllByRole("img")
+      expect(result).toHaveLength(5)
+      result.slice(0,3).forEach((value)=>{
+        expect(value).toHaveAttribute('src', expect.stringContaining('yellow'))
+      })
+      result.slice(3,6).forEach((value)=>{
+        expect(value).toHaveAttribute('src', expect.stringContaining('white'))
+      })
+    });
+    it('returns 4 full stars and 1 empty stars on rating=3.6 and roundRating=true', ()=>{
+      const rating=3.6
+      render(<StarsLine rating={rating} roundRating={true} />)
+      const result = screen.getAllByRole("img")
+      expect(result).toHaveLength(5)
+      result.slice(0,4).forEach((value)=>{
+        expect(value).toHaveAttribute('src', expect.stringContaining('yellow'))
+      })
+      result.slice(4,6).forEach((value)=>{
+        expect(value).toHaveAttribute('src', expect.stringContaining('white'))
+      })
+    });
 })
