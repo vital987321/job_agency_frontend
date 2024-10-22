b67b920b5ed155b68ba7e99729b080a227a9b4de
vital987321 <vitaliyvelichko@gmail.com>
2024-10-23 00:26:50 +0200

    Added AdminFilterControls.test.jsx

--- src/commonItems/features/adminFilterControls/AdminFilterControls.test.jsx
+++ src/commonItems/features/adminFilterControls/AdminFilterControls.test.jsx
@@ -1,11 +1,60 @@
 import { render, screen } from "@testing-library/react"
 import { AdminFilterControls } from "./AdminFilterControls"
-import { it, expect, describe } from 'vitest'
+import { it, expect, describe, vi } from 'vitest'
+import userEvent from "@testing-library/user-event";
 
 describe('AdminFilterControls', () => {
-    it('render filter button', () => {
-        render(<AdminFilterControls/>)
-        filterButton=screen.getByRole('button', {name: /Filter/i})
+    const filterButtonHandler=vi.fn();
+    const resetFiltersHandler=vi.fn();
+    const validProps={
+        filterButtonHandler,
+        resetFiltersHandler,
+        onPageListItemsAmount: '5',
+        onChangeListItemsAmount: vi.fn(),
+        displayResetFilterButton: false,
+    };
 
+    const propsWithReset={
+        ...validProps,
+        displayResetFilterButton: true
+    }
+
+    const renderComponent=(propsObject)=>{
+        render(<AdminFilterControls {...propsObject}/>)
+
+        return{
+            user: userEvent.setup(),
+            getFilterButton: ()=>screen.getByRole('button', {name: (content, element) => content.startsWith('Filter') }),
+            getResetFilterButton: ()=> screen.queryByRole('button', {name: /reset filter/i})
+        }
+    }
+
+    it('should render filter button', () => {
+        const {getFilterButton}=renderComponent(validProps)
+        expect (getFilterButton()).toBeInTheDocument()
     })
+    it('should render "on Page" component ', () => {
+        renderComponent(validProps)
+        const onPage=screen.getByLabelText(/on page/i)
+        expect (onPage).toBeInTheDocument()
+    })
+    it('should not render Reset Filter button if displayResetFilterButton=false', () => {
+        const {getResetFilterButton}=renderComponent(validProps)
+        expect(getResetFilterButton()).not.toBeInTheDocument()
+    })
+    it('should render Reset Filter button if displayResetFilterButton=true', () => {
+        const {getResetFilterButton}=renderComponent(propsWithReset)
+        expect(getResetFilterButton()).toBeInTheDocument()
+    })
+    it('should run handler on Filter button click', async () => {
+        const {user, getFilterButton}=renderComponent(validProps)
+        await user.click(getFilterButton())
+        expect(filterButtonHandler).toHaveBeenCalledOnce()
+    })
+    it('should run handler on Reset Filter button click', async () => {
+        const {user, getResetFilterButton}=renderComponent(propsWithReset)
+        await user.click(getResetFilterButton())
+        expect(resetFiltersHandler).toHaveBeenCalledOnce()
+    })
+
 })
